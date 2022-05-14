77f4026aa13b3230d9091c9b59091f10512f25af
lcmongwe <lcmongwe@gmail.com>
2022-05-14 10:04:28 +0300

    blog form and route created

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from unicodedata import category
 from flask import render_template,request,redirect,url_for,abort,flash
 
-from app.main.forms import CommentForm
+from app.main.forms import BLogForm, CommentForm
 from . import main
 
 # from ..models import  Pitch, User,Comment,Upvote,Downvote
@@ -10,15 +10,6 @@ from . import main
 from .. import db
 from flask.views import View,MethodView
 # import markdown2
-
-
-
-
-@main.route('/profile')
-def profile():
-    
-    return render_template("profile/profile.html")
-
 
 
 @main.route('/')
@@ -30,3 +21,20 @@ def index():
     return render_template('home.html', form=form)
 
 
+@main.route('/profile')
+def profile():
+    
+    return render_template("profile/profile.html")
+
+
+@main.route('/write')
+def write():
+    form=BLogForm()
+    
+    return render_template("writer/writer.html",blog_form=form)
+
+@main.route('/blogs')
+def blogs():
+    form=CommentForm()
+    
+    return render_template("allblogs.html",comment_form=form)
