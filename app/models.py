0a0b2b21ec302fc048a80fdbb28bc9613567715c
lcmongwe <lcmongwe@gmail.com>
2022-05-16 10:51:04 +0300

    comment model created

--- app/models.py
+++ app/models.py
@@ -23,6 +23,8 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255),unique = True,index = True)
     pass_secure = db.Column(db.String(255))
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
     
 
     @property
@@ -43,16 +45,31 @@ class User(UserMixin,db.Model):
 class Blog(db.Model):
     '''
     '''
-    __tablename__ = 'blog'
+    __tablename__ = 'blogs'
 
     id = db.Column(db.Integer, primary_key = True)
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable = False)
     description = db.Column(db.String(), index = True)
     category = db.Column(db.String(255), nullable=False)
-    comments = db.relationship('Comment',backref='blog',lazy='dynamic')
+    comments = db.relationship('Comment',backref='blogs',lazy='dynamic')
     
     
     @classmethod
     def get_blogs(cls, id):
         blogs = Blog.query.order_by(pitch_id=id).desc().all()
         return blogs
+
+    def __repr__(self):
+        return f'Blog {self.description}'
+
+class Comment(db.Model):
+    __tablename__='comments'
+    
+    id = db.Column(db.Integer,primary_key=True)
+    blog_id = db.Column(db.Integer, db.ForeignKey('blogs.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable= False)
+    description = db.Column(db.Text)
+
+    
+    def __repr__(self):
+        return f"Comment : id: {self.id} comment: {self.description}"
