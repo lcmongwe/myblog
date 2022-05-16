7316103552bfcb1724a84b9e065066a54f6973fc
lcmongwe <lcmongwe@gmail.com>
2022-05-16 10:55:19 +0300

    login form created

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,19 +8,29 @@ from wtforms import ValidationError
 
 class RegistrationForm(FlaskForm):
 
-    email = StringField('Your Email Address')
-    username = StringField('Enter your username')
-    password = PasswordField('Password')
-    EqualTo('password_confirm')
-    password_confirm = PasswordField('Confirm Passwords')
+    email = StringField('Your Email Address',validators=[InputRequired(),Email()])
+    username = StringField('Enter your username',validators = [InputRequired()])
+    password = PasswordField('Password',validators = [InputRequired(),
+    EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [InputRequired()])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('There is an account with that email')
+
+    def validate_username(self,data_field):
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('That username is taken')
+
 
     
 
 class LoginForm(FlaskForm):
-    email = StringField('Your Email Address')
-    password = PasswordField('Password')
+    email = StringField('Your Email Address',validators=[InputRequired(),Email()])
+    password = PasswordField('Password',validators =[InputRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+
 
 
