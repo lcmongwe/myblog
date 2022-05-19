c1f60ba9b9cbe7e3cdfcdbc981a9e8e5d231f620
lcmongwe <lcmongwe@gmail.com>
2022-05-19 09:57:52 +0300

    procfile created

--- manage.py
+++ manage.py
@@ -27,7 +27,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db=db,User=User)
+    return dict(app = app,db=db,User=User,Blog=Blog,Comment=Comment)
 
 
 if __name__ == '__main__':
