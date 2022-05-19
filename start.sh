2b116b704df35c7aa09047654d445f11c2366643
lcmongwe <lcmongwe@gmail.com>
2022-05-19 14:31:05 +0300

    installed requirements needed for heroku deployment

--- start.sh
+++ start.sh
@@ -4,4 +4,5 @@ export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://lucy:4444@localhost/myblog
 # python3 manage.py db migrate -m "Deployment Migration"
 # python3 manage.py db upgrade
 
-python3.8 manage.py server
+# python3.8 manage.py server
+python3.8 manage.py shell
